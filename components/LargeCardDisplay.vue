b7c99da7871fabd9770985808d22cf9d1e684cf1
Raymond <rayjxclusive@gmail.com>
2021-10-15 11:04:52 +0300

    render LardCard components

--- components/LargeCardDisplay.vue
+++ components/LargeCardDisplay.vue
@@ -7,7 +7,11 @@
     </div>
 
     <div class="cards-container">
-
+      <LargeCard
+      v-for="card in cardsSection.cards"
+      :key="card.image"
+      :card="card"
+      />
     </div>
   </div>
 </template>
