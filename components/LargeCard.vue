74dd7c6483b0efc636159af1a68e0138a5a27b08
Raymond <rayjxclusive@gmail.com>
2021-10-16 00:32:05 +0300

    update add link to the large card

--- components/LargeCard.vue
+++ components/LargeCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="card">
+  <NuxtLink :to ="`/products/${card.id}`" class="card">
     <img class="image" :src="require(`@/assets/images/${card.image}`)" alt="" />
     <h3 class="header"> {{card.title}} </h3>
     <p class="snippet"> {{card.snippet}} </p>
-  </div>
+  </NuxtLink>
 </template>
 
 <script>
