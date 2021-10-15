a387f73306000237f4ed9e9f8c7eb02203cfe233
Raymond <rayjxclusive@gmail.com>
2021-10-15 10:15:16 +0300

    update fix font size on header

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Hero container">
       <div class="text-container">
-          <h1 enter-class="header">Find your Fire Extinguisher</h1>
+          <h1 class="header">Find your Fire Extinguisher</h1>
           <p class="snippet">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste iusto illum quidem asperiores, cum nisi aperiam eius ipsa sapiente odit!</p>
           <button class="button btn">Start button</button>
       </div>
---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h4>
+        Top-rated Fire Extinguisher in Nigara 
+    </h4>
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style  scoped>
+
+</style>
